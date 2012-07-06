3c0f5c7be4e4b0ab7e51034d050ec5ff5874910c
Frog <Frog@Frog-PC>
2012-07-06 20:27:53 +0900

    first commit

---
+++ src/com/rm/test8/Test8Activity.java
@@ -0,0 +1,13 @@
+package com.rm.test8;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Test8Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
